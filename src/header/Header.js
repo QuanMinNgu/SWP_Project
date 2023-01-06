fa93ad182a90e52fbcb170be90301eaa11e12460
QuangNM <quangnmhe160393@fpt.edu.vn>
2023-01-06 19:53:55 +0700

    update header

--- src/header/Header.js
+++ src/header/Header.js
@@ -6,6 +6,7 @@ import AnnouceCard from "../card/AnnouceCard";
 const Header = () => {
     const [course, setCourse] = useState(false);
     const [annouce, setAnnouce] = useState(false);
+    const [sign, setSign] = useState(true);
 
     return (
         <div className="header">
@@ -29,62 +30,70 @@ const Header = () => {
                     />
                 </div>
             </div>
-            <div className="header_navbar">
-                <div className="header_navbar_items">
-                    <span className="header_navbar_items_title">
-                        <button
-                            onClick={() => {
-                                setCourse(!course);
-                                setAnnouce(false);
-                            }}
-                        >
-                            Khóa học của tôi
-                        </button>
-                        {course && (
-                            <div className="header_navbar_items_course">
-                                <div className="header_navbar_items_course_title">
-                                    <h5>Khóa học của tôi</h5>
+            {sign ? (
+                <div className="header_navbar">
+                    <div className="header_navbar_items">
+                        <span className="header_navbar_items_title">
+                            <button
+                                onClick={() => {
+                                    setCourse(!course);
+                                    setAnnouce(false);
+                                }}
+                            >
+                                Khóa học của tôi
+                            </button>
+                            {course && (
+                                <div className="header_navbar_items_course">
+                                    <div className="header_navbar_items_course_title">
+                                        <h5>Khóa học của tôi</h5>
+                                    </div>
+                                    <div className="header_navbar_items_course_card">
+                                        <CourseHeadCard />
+                                    </div>
                                 </div>
-                                <div className="header_navbar_items_course_card">
-                                    <CourseHeadCard />
+                            )}
+                        </span>
+                    </div>
+                    <div className="header_navbar_items header_navbar_items_bell">
+                        <i
+                            onClick={() => {
+                                setAnnouce(!annouce);
+                                setCourse(false);
+                            }}
+                            className="fa-solid fa-bell"
+                        ></i>
+                        {annouce && (
+                            <div className="header_navbar_items_bell_noti">
+                                <div className="header_navbar_items_bell_noti_title">
+                                    <h6>Thông Báo</h6>
+                                    <div>
+                                        <i className="fa-solid fa-ellipsis"></i>
+                                    </div>
+                                </div>
+                                <div className="header_navbar_items_bell_noti_card">
+                                    <AnnouceCard />
+                                    <AnnouceCard />
                                 </div>
                             </div>
                         )}
-                    </span>
-                </div>
-                <div className="header_navbar_items header_navbar_items_bell">
-                    <i
-                        onClick={() => {
-                            setAnnouce(!annouce);
-                            setCourse(false);
-                        }}
-                        className="fa-solid fa-bell"
-                    ></i>
-                    {annouce && (
-                        <div className="header_navbar_items_bell_noti">
-                            <div className="header_navbar_items_bell_noti_title">
-                                <h6>Thông Báo</h6>
-                                <div>
-                                    <i className="fa-solid fa-ellipsis"></i>
-                                </div>
-                            </div>
-                            <div className="header_navbar_items_bell_noti_card">
-                                <AnnouceCard />
-                                <AnnouceCard />
-                            </div>
+                    </div>
+                    <div className="header_navbar_items">
+                        <div className="header_navbar_items_img_wrap">
+                            <img
+                                className="header_navbar_items_img"
+                                src="https://phunugioi.com/wp-content/uploads/2020/02/mau-background-dep.jpg"
+                                alt="Ảnh"
+                            />
                         </div>
-                    )}
-                </div>
-                <div className="header_navbar_items">
-                    <div className="header_navbar_items_img_wrap">
-                        <img
-                            className="header_navbar_items_img"
-                            src="https://phunugioi.com/wp-content/uploads/2020/02/mau-background-dep.jpg"
-                            alt="Ảnh"
-                        />
                     </div>
                 </div>
-            </div>
+            ) : (
+                <div className="header_navbar">
+                    <Link style={{ textDecoration: "none" }} to="/">
+                        <div className="header_navbar_button">Đăng Nhập</div>
+                    </Link>
+                </div>
+            )}
         </div>
     );
 };
