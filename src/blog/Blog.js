97db60875ab7e6a1ea4bfb7481aad33c71d0aaaa
QuangNM <quangnmhe160393@fpt.edu.vn>
2023-01-09 08:12:20 +0700

    adding blog for edit

---
+++ src/blog/Blog.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./style.scss";
+const Blog = () => {
+    return <div>Blog</div>;
+};
+
+export default Blog;
---
+++ src/card/BlogCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./style.scss";
+const BlogCard = () => {
+    return <div>BlogCard</div>;
+};
+
+export default BlogCard;
