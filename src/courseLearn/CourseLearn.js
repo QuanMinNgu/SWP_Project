6d3072a3ed72028afa99c2a9a0e8dac2880d90fc
QuangNM <quangnmhe160393@fpt.edu.vn>
2023-01-12 08:36:13 +0700

    adding more feature

--- src/courseLearn/CourseLearn.js
+++ src/courseLearn/CourseLearn.js
@@ -2,10 +2,18 @@ import React, { useState } from "react";
 import "./style.scss";
 import { Link } from "react-router-dom";
 import CourseLearnCard from "./CourseLearnCard";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 const CourseLearn = () => {
     const [percent, setPercent] = useState(86 * 3.6);
     const style = {
         background: `conic-gradient(#F05123 ${percent}deg,transparent 0deg)`,
+    };
+
+    const initialOptions = {
+        "client-id":
+            "AQVdNJ2adELwM1B1LTHKG8rcc-MKrGUU9g4SLcy3SYuAyYukSyCU7BFEZz7ix0nIaFksX0AeZrWkR8-h",
+        currency: "USD",
+        intent: "capture",
     };
     return (
         <div className="CourseLearn">
@@ -78,6 +86,37 @@ const CourseLearn = () => {
                                     https://www.youtube.com/F8VNOfficial Sơn
                                     Đặng: https://www.facebook.com/sondnf8
                                 </div>
+                                <PayPalScriptProvider options={initialOptions}>
+                                    <PayPalButtons
+                                        createOrder={(data, actions) => {
+                                            return actions.order.create({
+                                                purchase_units: [
+                                                    {
+                                                        description:
+                                                            "Oki for looking good",
+                                                        amount: {
+                                                            value: "0.1",
+                                                        },
+                                                    },
+                                                ],
+                                            });
+                                        }}
+                                        onApprove={async (data, actions) => {
+                                            const order =
+                                                await actions.order.capture();
+                                            console.log(order);
+                                            alert(
+                                                "Thank you for your purchasing."
+                                            );
+                                        }}
+                                        onCancel={() => {
+                                            alert("You canceled.");
+                                        }}
+                                        onError={() => {
+                                            alert("Paypal checkout on error.");
+                                        }}
+                                    />
+                                </PayPalScriptProvider>
                             </div>
                         </div>
                     </div>
