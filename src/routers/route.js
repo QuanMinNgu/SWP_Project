7cfd7db3cddae98b95545ee9eacf3167e16a2a46
DiorEtMoi <dinhhoan0019c@gmail.com>
2023-01-26 22:05:52 +0700

    adding profile

--- src/routers/route.js
+++ src/routers/route.js
@@ -8,47 +8,53 @@ import CourseDetail from "../courses/CourseDetail";
 import Blog from "../blog/Blog";
 import CourseLearn from "../courseLearn/CourseLearn";
 import BlogDetail from "../blog/BlogDetail";
+import Profile from "../user/Profile";
 export const publicRouter = [
-    {
-        element: Home,
-        path: "/",
-        layout: DefaultLayout,
-    },
-    {
-        element: Login,
-        path: "/login",
-    },
-    {
-        element: Blog,
-        path: "/blog",
-        layout: DefaultLayout,
-    },
-    {
-        element: Register,
-        path: "/register",
-    },
-    {
-        element: ForgotPassword,
-        path: "/forgot_password",
-    },
-    {
-        element: ChangePassword,
-        path: "/change_password",
-    },
-    {
-        element: CourseLearn,
-        path: "/learning/:slug",
-    },
-    {
-        element: CourseDetail,
-        path: "/course/:slug",
-        layout: DefaultLayout,
-    },
-    ,
-    {
-        element: BlogDetail,
-        path: "/blog/:slug",
-        layout: DefaultLayout,
-    },
+  {
+    element: Home,
+    path: "/",
+    layout: DefaultLayout,
+  },
+  {
+    element: Login,
+    path: "/login",
+  },
+  {
+    element: Blog,
+    path: "/blog",
+    layout: DefaultLayout,
+  },
+  {
+    element: Register,
+    path: "/register",
+  },
+  {
+    element: ForgotPassword,
+    path: "/forgot_password",
+  },
+  {
+    element: ChangePassword,
+    path: "/change_password",
+  },
+  {
+    element: CourseLearn,
+    path: "/learning/:slug",
+  },
+  {
+    element: CourseDetail,
+    path: "/course/:slug",
+    layout: DefaultLayout,
+  },
+  ,
+  {
+    element: BlogDetail,
+    path: "/blog/:slug",
+    layout: DefaultLayout,
+  },
+  {
+    element: Profile,
+    path: "/user/profile",
+    layout: DefaultLayout,
+  },
 ];
 export const privateRouter = [];
