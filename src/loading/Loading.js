62c6cbe0ecb354aa1001aaf3ed638144cad17772
QuangNM <quangnmhe160393@fpt.edu.vn>
2023-02-06 13:13:09 +0700

    adding loading

---
+++ src/loading/Loading.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css";
+const Loading = () => {
+    return (
+        <div className="loading_container">
+            <div className="loader">
+                <div
+                    style={{ borderColor: "white" }}
+                    className="inner one"
+                ></div>
+                <div style={{ borderColor: "red" }} className="inner two"></div>
+                <div className="inner three"></div>
+            </div>
+        </div>
+    );
+};
+
+export default Loading;
