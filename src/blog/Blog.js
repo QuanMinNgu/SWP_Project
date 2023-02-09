e1c393011613f2e3ec092070b1cae441e0827aaf
QuangNM <quangnmhe160393@fpt.edu.vn>
2023-02-09 12:49:28 +0700

    updating header

--- src/blog/Blog.js
+++ src/blog/Blog.js
@@ -2,37 +2,37 @@ import React, { useEffect } from "react";
 import BlogCard from "../card/BlogCard";
 import "./style.scss";
 const Blog = () => {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-  return (
-    <div className="blog_container">
-      <div className="blog_container_header">
-        <div className="blog_container_header_left">
-          <h2>Bài viết của tôi</h2>
-          <p>Chia sẻ bài viết hay tại đây</p>
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+    return (
+        <div className="blog_container">
+            <div className="blog_container_header">
+                <div className="blog_container_header_left">
+                    <h2>Bài viết của tôi</h2>
+                    <p>Chia sẻ bài viết hay tại đây</p>
+                </div>
+                <div className="blog_container_header_right">
+                    <div className="blog_container_header_right_input">
+                        <input type="text" placeholder="Tìm kiếm blog" />
+                        <button>
+                            <i className="fa-solid fa-magnifying-glass"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className="blog_container_body">
+                <div className="blog_container_body_cards">
+                    <BlogCard />
+                    <BlogCard />
+                    <BlogCard />
+                    <BlogCard />
+                    <BlogCard />
+                    <BlogCard />
+                </div>
+            </div>
         </div>
-        <div className="blog_container_header_right">
-          <div className="blog_container_header_right_input">
-            <input type="text" placeholder="Tìm kiếm blog" />
-            <button>
-              <i className="fa-solid fa-magnifying-glass"></i>
-            </button>
-          </div>
-        </div>
-      </div>
-      <div className="blog_container_body">
-        <div className="blog_container_body_cards">
-          <BlogCard />
-          <BlogCard />
-          <BlogCard />
-          <BlogCard />
-          <BlogCard />
-          <BlogCard />
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Blog;
--- src/user/Profile.js
+++ src/user/Profile.js
@@ -3,94 +3,97 @@ import { useState } from "react";
 import ProfileCard from "./ProfileCard";
 import "./style.scss";
 const Profile = () => {
-  const [backGround, setBackground] = useState(
-    "https://fullstack.edu.vn/static/media/cover-profile.3fb9fed576da4b28386a.png"
-  );
-  const inputRef = useRef();
-  const [openNav, setOpenNav] = useState(false);
-  const handleOpen = () => {
-    setOpenNav(!openNav);
-  };
-  const handleClickOpen = () => {
-    inputRef.current.click();
-  };
-  const handleImg = (e) => {
-    const file = e.target.files[0];
-    setBackground(URL.createObjectURL(file));
-  };
-  useEffect(() => {
-    return () => {
-      URL.revokeObjectURL(backGround);
+    const [backGround, setBackground] = useState(
+        "https://fullstack.edu.vn/static/media/cover-profile.3fb9fed576da4b28386a.png"
+    );
+    const inputRef = useRef();
+    const [openNav, setOpenNav] = useState(false);
+    const handleOpen = () => {
+        setOpenNav(!openNav);
     };
-  }, [backGround]);
+    const handleClickOpen = () => {
+        inputRef.current.click();
+    };
+    const handleImg = (e) => {
+        const file = e.target.files[0];
+        setBackground(URL.createObjectURL(file));
+    };
+    useEffect(() => {
+        return () => {
+            URL.revokeObjectURL(backGround);
+        };
+    }, [backGround]);
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-  return (
-    <div className="profile">
-      <div
-        className="profile_header"
-        style={{
-          backgroundImage: `url(${backGround})`,
-        }}
-      >
-        <div className="profile_header_user">
-          <img src="https://i.pinimg.com/564x/26/3a/d5/263ad55f3fc6f594d8f1c91d2c396a48.jpg" />
-          <div>
-            <h2>Dinh Hoan</h2>
-          </div>
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+    return (
+        <div className="profile">
+            <div
+                className="profile_header"
+                style={{
+                    backgroundImage: `url(${backGround})`,
+                }}
+            >
+                <div className="profile_header_user">
+                    <img src="https://i.pinimg.com/564x/26/3a/d5/263ad55f3fc6f594d8f1c91d2c396a48.jpg" />
+                    <div>
+                        <h2>Dinh Hoan</h2>
+                    </div>
+                </div>
+                <div className="profile_header_fix" onClick={handleOpen}>
+                    <div className="profile_header_fix_icon">
+                        <i className="fa-solid fa-camera"></i>
+                    </div>
+                    <div className="profile_header_fix_text">
+                        <h3>Chỉnh sửa ảnh bìa</h3>
+                    </div>
+                </div>
+                {openNav && (
+                    <div
+                        className="profile_header_change"
+                        onClick={handleClickOpen}
+                    >
+                        <i className="fa-solid fa-upload"></i>
+                        <h3>Tải ảnh lên</h3>
+                        <input
+                            ref={inputRef}
+                            type="file"
+                            style={{
+                                display: "none",
+                            }}
+                            onChange={handleImg}
+                        />
+                    </div>
+                )}
+            </div>
+            <div className="profile_container">
+                <div className="profile_container_courses_done">
+                    <div className="profile_container_courses_title">
+                        <h3>Các khóa học đã hoàn thành</h3>
+                    </div>
+                    <div className="profile_container_courses_card">
+                        <ProfileCard />
+                        <ProfileCard />
+                        <ProfileCard />
+                        <ProfileCard />
+                        <ProfileCard />
+                    </div>
+                </div>
+                <div className="profile_container_courses_done">
+                    <div className="profile_container_courses_title">
+                        <h3>Các khóa học đang học</h3>
+                    </div>
+                    <div className="profile_container_courses_card">
+                        <ProfileCard />
+                        <ProfileCard />
+                        <ProfileCard />
+                        <ProfileCard />
+                        <ProfileCard />
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="profile_header_fix" onClick={handleOpen}>
-          <div className="profile_header_fix_icon">
-            <i className="fa-solid fa-camera"></i>
-          </div>
-          <div className="profile_header_fix_text">
-            <h3>Chỉnh sửa ảnh bìa</h3>
-          </div>
-        </div>
-        {openNav && (
-          <div className="profile_header_change" onClick={handleClickOpen}>
-            <i className="fa-solid fa-upload"></i>
-            <h3>Tải ảnh lên</h3>
-            <input
-              ref={inputRef}
-              type="file"
-              style={{
-                display: "none",
-              }}
-              onChange={handleImg}
-            />
-          </div>
-        )}
-      </div>
-      <div className="profile_container">
-        <div className="profile_container_courses_done">
-          <div className="profile_container_courses_title">
-            <h3>Các khóa học đã hoàn thành</h3>
-          </div>
-          <div className="profile_container_courses_card">
-            <ProfileCard />
-            <ProfileCard />
-            <ProfileCard />
-            <ProfileCard />
-            <ProfileCard />
-          </div>
-        </div>
-        <div className="profile_container_courses_done">
-          <div className="profile_container_courses_title">
-            <h3>Các khóa học đang học</h3>
-          </div>
-          <div className="profile_container_courses_card">
-            <ProfileCard />
-            <ProfileCard />
-            <ProfileCard />
-            <ProfileCard />
-            <ProfileCard />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 export default Profile;
