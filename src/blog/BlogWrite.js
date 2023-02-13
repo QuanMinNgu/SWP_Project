f8e48f3e7326a9746c6dc40661e836b357cc0e40
QuangNM <quangnmhe160393@fpt.edu.vn>
2023-02-13 18:10:17 +0700

    updating create blog api

--- src/blog/BlogWrite.js
+++ src/blog/BlogWrite.js
@@ -3,11 +3,15 @@ import "./style.scss";
 import { Editor } from "react-draft-wysiwyg";
 import { EditorState, convertToRaw } from "draft-js";
 import draftToHtml from "draftjs-to-html";
-
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import { isSuccess, isLoading, isFailing } from ".././redux/slice/auth";
+import axios from "axios";
 const BlogWrite = () => {
     const [editorState, setEditorState] = useState(EditorState.createEmpty());
 
     const [title, setTitle] = useState("");
+    const [meta, setMeta] = useState("");
     const [content, setContent] = useState("");
     const handleChange = (data) => {
         setEditorState(data);
@@ -17,9 +21,37 @@ const BlogWrite = () => {
         window.scrollTo(0, 0);
     }, []);
 
+    const auth = useSelector((state) => state.auth);
+    const dispatch = useDispatch();
     useEffect(() => {
         setContent(draftToHtml(convertToRaw(editorState.getCurrentContent())));
     }, [editorState]);
+
+    const handleCreateNewBlog = async () => {
+        if (!title || !meta || !content) {
+            return toast.error("Vui lòng điền hết thông tin.");
+        }
+        dispatch(isLoading());
+        console.log({
+            token: auth.user?.accessToken,
+            title: title,
+            meta: meta,
+            content: content,
+        });
+        try {
+            const data = await axios.post("/api/blog/create", {
+                token: auth.user?.accessToken,
+                title: title,
+                meta: meta,
+                content: content,
+            });
+            toast.success(data?.data?.msg);
+            dispatch(isSuccess());
+        } catch (err) {
+            toast.error(err?.response?.data?.msg);
+            dispatch(isFailing());
+        }
+    };
 
     return (
         <div className="newPost">
@@ -32,6 +64,20 @@ const BlogWrite = () => {
                     }}
                 ></div>
                 {!title && <div className="newPost_title_content">Tiêu đề</div>}
+            </div>
+            <div className="newPost_title">
+                <div
+                    className="newPost_title_edit_meta"
+                    contentEditable={true}
+                    onInput={(e) => {
+                        setMeta(e.target.innerHTML);
+                    }}
+                ></div>
+                {!meta && (
+                    <div className="newPost_title_content_meta">
+                        Nội dung nhỏ
+                    </div>
+                )}
             </div>
             <div className="newPost_content">
                 <Editor
@@ -49,7 +95,7 @@ const BlogWrite = () => {
                 )}
             </div>
             <div className="newPost_update">
-                <button>
+                <button onClick={handleCreateNewBlog}>
                     <i
                         style={{ marginRight: "0.5rem", fontSize: "1.3rem" }}
                         className="fa-solid fa-upload"
