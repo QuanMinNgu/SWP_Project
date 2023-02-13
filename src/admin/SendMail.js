e85fe84119510a3b15a5181f354d5a9c0f948b50
QuangNM <quangnmhe160393@fpt.edu.vn>
2023-02-13 09:32:36 +0700

    updating api for dashboard

--- src/admin/SendMail.js
+++ src/admin/SendMail.js
@@ -1,7 +1,36 @@
 import React from "react";
-
+import "./style.scss";
 const SendMail = () => {
-    return <div>SendMail</div>;
+    return (
+        <div className="managerCourse">
+            <div className="sendMail_title">
+                <div className="sendMail_title_items">
+                    <input
+                        id="individual"
+                        defaultChecked
+                        type="radio"
+                        value="individual"
+                        name="sendmail"
+                    />
+                    <label htmlFor="individual">
+                        <i>Individual</i>
+                    </label>
+                </div>
+                <div className="sendMail_title_items">
+                    <input
+                        id="alreadyform"
+                        type="radio"
+                        value="alreadyform"
+                        name="sendmail"
+                    />
+                    <label htmlFor="alreadyform">
+                        <i>Already form</i>
+                    </label>
+                </div>
+            </div>
+            <div className="sendMail_user_form"></div>
+        </div>
+    );
 };
 
 export default SendMail;
