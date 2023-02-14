43497a87bc8253b56d15807a575f52e0177c67b4
DiorEtMoi <dinhhoan0019c@gmail.com>
2023-02-14 22:30:25 +0700

    updating api blog

--- src/blog/BlogWrite.js
+++ src/blog/BlogWrite.js
@@ -8,103 +8,98 @@ import { toast } from "react-toastify";
 import { isSuccess, isLoading, isFailing } from ".././redux/slice/auth";
 import axios from "axios";
 const BlogWrite = () => {
-    const [editorState, setEditorState] = useState(EditorState.createEmpty());
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
 
-    const [title, setTitle] = useState("");
-    const [meta, setMeta] = useState("");
-    const [content, setContent] = useState("");
-    const handleChange = (data) => {
-        setEditorState(data);
-    };
+  const [title, setTitle] = useState("");
+  const [meta, setMeta] = useState("");
+  const [content, setContent] = useState("");
+  const handleChange = (data) => {
+    setEditorState(data);
+  };
 
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, []);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-    const auth = useSelector((state) => state.auth);
-    const dispatch = useDispatch();
-    useEffect(() => {
-        setContent(draftToHtml(convertToRaw(editorState.getCurrentContent())));
-    }, [editorState]);
+  const auth = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    setContent(draftToHtml(convertToRaw(editorState.getCurrentContent())));
+  }, [editorState]);
 
-    const handleCreateNewBlog = async () => {
-        if (!title || !meta || !content) {
-            return toast.error("Vui lòng điền hết thông tin.");
-        }
-        dispatch(isLoading());
-        console.log({
-            token: auth.user?.accessToken,
-            title: title,
-            meta: meta,
-            content: content,
-        });
-        try {
-            const data = await axios.post("/api/blog/create", {
-                token: auth.user?.accessToken,
-                title: title,
-                meta: meta,
-                content: content,
-            });
-            toast.success(data?.data?.msg);
-            dispatch(isSuccess());
-        } catch (err) {
-            toast.error(err?.response?.data?.msg);
-            dispatch(isFailing());
-        }
-    };
+  const handleCreateNewBlog = async () => {
+    if (!title || !meta || !content) {
+      return toast.error("Vui lòng điền hết thông tin.");
+    }
+    dispatch(isLoading());
+    console.log({
+      token: auth.user?.accessToken,
+      title: title,
+      meta: meta,
+      content: content,
+    });
+    try {
+      const data = await axios.post("/api/blog/create", {
+        token: auth.user?.accessToken,
+        blogName: title,
+        blogMeta: meta,
+        content: content,
+      });
+      toast.success(data?.data?.msg);
+      dispatch(isSuccess());
+    } catch (err) {
+      toast.error(err?.response?.data?.msg);
+      dispatch(isFailing());
+    }
+  };
 
-    return (
-        <div className="newPost">
-            <div className="newPost_title">
-                <div
-                    className="newPost_title_edit"
-                    contentEditable={true}
-                    onInput={(e) => {
-                        setTitle(e.target.innerHTML);
-                    }}
-                ></div>
-                {!title && <div className="newPost_title_content">Tiêu đề</div>}
-            </div>
-            <div className="newPost_title">
-                <div
-                    className="newPost_title_edit_meta"
-                    contentEditable={true}
-                    onInput={(e) => {
-                        setMeta(e.target.innerHTML);
-                    }}
-                ></div>
-                {!meta && (
-                    <div className="newPost_title_content_meta">
-                        Nội dung nhỏ
-                    </div>
-                )}
-            </div>
-            <div className="newPost_content">
-                <Editor
-                    editorState={editorState}
-                    onEditorStateChange={handleChange}
-                    wrapperClassName="editor-wrapper"
-                    editorClassName="message-editor"
-                    toolbarClassName="message-toolbar"
-                />
-                {!convertToRaw(editorState.getCurrentContent())?.blocks[0]
-                    ?.text && (
-                    <div className="newPost_content_title">
-                        Nội dung viết tại đây
-                    </div>
-                )}
-            </div>
-            <div className="newPost_update">
-                <button onClick={handleCreateNewBlog}>
-                    <i
-                        style={{ marginRight: "0.5rem", fontSize: "1.3rem" }}
-                        className="fa-solid fa-upload"
-                    ></i>
-                    Đăng
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="newPost">
+      <div className="newPost_title">
+        <div
+          className="newPost_title_edit"
+          contentEditable={true}
+          onInput={(e) => {
+            setTitle(e.target.innerHTML);
+          }}
+        ></div>
+        {!title && <div className="newPost_title_content">Tiêu đề</div>}
+      </div>
+      <div className="newPost_title">
+        <div
+          className="newPost_title_edit_meta"
+          contentEditable={true}
+          onInput={(e) => {
+            setMeta(e.target.innerHTML);
+          }}
+        ></div>
+        {!meta && (
+          <div className="newPost_title_content_meta">Nội dung nhỏ</div>
+        )}
+      </div>
+      <div className="newPost_content">
+        <Editor
+          editorState={editorState}
+          onEditorStateChange={handleChange}
+          wrapperClassName="editor-wrapper"
+          editorClassName="message-editor"
+          toolbarClassName="message-toolbar"
+        />
+        {!convertToRaw(editorState.getCurrentContent())?.blocks[0]?.text && (
+          <div className="newPost_content_title">Nội dung viết tại đây</div>
+        )}
+      </div>
+      <div className="newPost_update">
+        <button onClick={handleCreateNewBlog}>
+          <i
+            style={{ marginRight: "0.5rem", fontSize: "1.3rem" }}
+            className="fa-solid fa-upload"
+          ></i>
+          Đăng
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default BlogWrite;
