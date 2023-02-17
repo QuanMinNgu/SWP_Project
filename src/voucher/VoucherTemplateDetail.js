b924ee8d68d158917faa85f7643dac3bdfe2da19
QuangNM <quangnmhe160393@fpt.edu.vn>
2023-02-17 12:57:32 +0700

    updating

--- src/voucher/VoucherTemplateDetail.js
+++ src/voucher/VoucherTemplateDetail.js
@@ -1,29 +1,32 @@
 import React from "react";
 import "./style.scss";
 function VoucherTemplateDetail() {
-  return (
-    <div className="voucher_template">
-      <div className="voucher_template_top">
-        <h2>Gift Voucher</h2>
-      </div>
-      <div className="voucher_template_body">
-        <h2 className="v_name">Name :</h2>
-        <h2 className="v_name">Description :</h2>
-        <h2 className="v_name">Amount :</h2>
+	return (
+		<div className="voucher_template">
+			<div className="voucher_template_top">
+				<h2>Gift Voucher</h2>
+			</div>
+			<div className="voucher_template_body">
+				<h2 className="v_name">Name : This is for newbier</h2>
+				<h2 className="v_name v_description">
+					Description : This is for newbier This is for newbier This is for
+					newbier
+				</h2>
+				<h2 className="v_name">Amount : $120</h2>
 
-        <div
-          style={{
-            display: "flex",
-            width: "100%",
-            flexWrap: "wrap",
-          }}
-        >
-          <h2 className="t_time">From : </h2>
-          <h2 className="t_time">To : </h2>
-        </div>
-      </div>
-    </div>
-  );
+				<div
+					style={{
+						display: "flex",
+						width: "100%",
+						flexWrap: "wrap",
+					}}
+				>
+					<h2 className="t_time">From : 11/11/2022</h2>
+					<h2 className="t_time">To : 11/11/2023</h2>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default VoucherTemplateDetail;
