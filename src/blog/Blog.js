ebd00829499fbff821c280daaa4d4ece24c0a333
DiorEtMoi <dinhhoan0019c@gmail.com>
2023-02-25 18:51:18 +0700

    updatig voucher

--- src/blog/Blog.js
+++ src/blog/Blog.js
@@ -1,60 +1,80 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
+import { useLocation } from "react-router-dom";
 import { toast } from "react-toastify";
+import { UserContext } from "../App";
 import BlogCard from "../card/BlogCard";
 import Pagination from "../paginating/Pagination";
 import { isFailing, isLoading, isSuccess } from "../redux/slice/auth";
 import "./style.scss";
 const Blog = () => {
-	const dispatch = useDispatch();
-	const [listBLog, setListBlog] = useState([]);
-	useEffect(() => {
-		window.scrollTo(0, 0);
-	}, []);
-	useEffect(() => {
-		const axiosGetAllBlog = async () => {
-			dispatch(isLoading());
-			try {
-				const res = await axios.get("/api/blog?page=1&limit=20");
-				setListBlog(res?.data);
-				console.log(res?.data);
-				dispatch(isSuccess());
-			} catch (err) {
-				dispatch(isFailing());
-				return toast.error(err?.response?.date?.msg);
-			}
-		};
-		axiosGetAllBlog();
-	}, []);
-	return (
-		<div className="blog_container">
-			<div className="blog_container_header">
-				<div className="blog_container_header_left">
-					<h2 style={{ fontSize: "4rem" }}>Blog</h2>
-					<p>Share good blog here</p>
-				</div>
-				<div className="blog_container_header_right">
-					<div className="blog_container_header_right_input">
-						<input type="text" placeholder="Tìm kiếm blog" />
-						<button>
-							<i className="fa-solid fa-magnifying-glass"></i>
-						</button>
-					</div>
-				</div>
-			</div>
-			<div className="blog_container_body">
-				<div className="blog_container_body_cards">
-					{listBLog?.blogs?.map((item, index) => {
-						return <BlogCard item={item} key={index} />;
-					})}
-				</div>
-			</div>
-			<div className="pagination blog_page">
-				<Pagination count={listBLog?.numPage} />
-			</div>
-		</div>
-	);
+  const { search } = useLocation();
+  const dispatch = useDispatch();
+  const [listBLog, setListBlog] = useState([]);
+  const { cache } = useContext(UserContext);
+  const page = new URLSearchParams(search).get("page") || 1;
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  useEffect(() => {
+    let here = true;
+    const url = `/api/blog?page=${page}&limit=20`;
+    if (cache.current[url]) {
+      return setListBlog(cache.current[url]);
+    }
+    dispatch(isLoading());
+    axios
+      .get(url)
+      .then((res) => {
+        if (!here) {
+          return dispatch(isSuccess());
+        }
+        setListBlog(res?.data);
+        cache.current[url] = res?.data;
+        console.log(res?.data);
+        dispatch(isSuccess());
+      })
+      .catch((err) => {
+        if (!here) {
+          return dispatch(isFailing());
+        }
+        dispatch(isFailing());
+        toast.error(err?.response?.data?.msg);
+      });
+    return () => {
+      here = false;
+    };
+  }, []);
+
+  return (
+    <div className="blog_container">
+      <div className="blog_container_header">
+        <div className="blog_container_header_left">
+          <h2 style={{ fontSize: "4rem" }}>Blog</h2>
+          <p>Share good blog here</p>
+        </div>
+        <div className="blog_container_header_right">
+          <div className="blog_container_header_right_input">
+            <input type="text" placeholder="Tìm kiếm blog" />
+            <button>
+              <i className="fa-solid fa-magnifying-glass"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="blog_container_body">
+        <div className="blog_container_body_cards">
+          {listBLog?.blogs?.map((item, index) => {
+            return <BlogCard item={item} key={index} />;
+          })}
+        </div>
+      </div>
+      <div className="pagination blog_page">
+        <Pagination count={listBLog?.numPage} />
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
--- src/voucher/VoucherTemplate.js
+++ src/voucher/VoucherTemplate.js
@@ -1,26 +1,33 @@
 import React, { useState } from "react";
 import "./style.scss";
-function VoucherTemplate() {
-	return (
-		<div className="voucher_template vocher_template_detail">
-			<div className="voucher_template_top">
-				<h2>Gift Voucher</h2>
-			</div>
-			<div className="voucher_template_body">
-				<h2 className="v_name">Amount : $120</h2>
-				<div
-					style={{
-						display: "flex",
-						width: "100%",
-						flexWrap: "wrap",
-					}}
-				>
-					<h2 className="t_time">From : 12/12/2022</h2>
-					<h2 className="t_time">To : 01/03/2023</h2>
-				</div>
-			</div>
-		</div>
-	);
+function VoucherTemplate({ onClick }) {
+  return (
+    <div className="voucher_new" onClick={onClick}>
+      <div className="voucher_new_left">
+        <div className="voucher_new_left_header">
+          <h3>Voucher</h3>
+        </div>
+        <div className="voucher_new_left_body">
+          <div className="voucher_new_left_body_time">
+            <p>Valid Until</p>
+            <span>23 June 2023</span>
+          </div>
+          <div className="voucher_new_left_body_des">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+              vulputate tellus ut magna vehicula lobortis.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="voucher_new_left_right">
+        <div className="voucher_new_left_right_header">
+          <p>Value</p>
+          <h3>25$</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default VoucherTemplate;
