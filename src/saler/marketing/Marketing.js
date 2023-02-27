946d9937763850675d046a142c11e2f42904dcb8
QuangNM <quangnmhe160393@fpt.edu.vn>
2023-02-27 13:03:16 +0700

    updating

--- src/saler/marketing/Marketing.js
+++ src/saler/marketing/Marketing.js
@@ -1,11 +1,19 @@
-import React, { useCallback, useRef, useState } from "react";
+import axios from "axios";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { useDropzone } from "react-dropzone";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import SwiperJs from "../../swiper/SwiperJs";
 import MarketingCard from "./MarketingCard";
 import "./style.scss";
+import { isFailing, isLoading, isSuccess } from "../../redux/slice/auth";
 const Marketing = () => {
 	const navigate = useNavigate();
+
+	const dispatch = useDispatch();
+
+	const auth = useSelector((state) => state.auth);
 
 	const [image, setImage] = useState("");
 	const imageRef = useRef();
@@ -20,6 +28,49 @@ const Marketing = () => {
 	const { getRootProps, getInputProps } = useDropzone({
 		onDrop,
 	});
+
+	const handleCreateMarketingImage = async () => {
+		const formData = new FormData();
+		const urlImage = "";
+		formData.append("file", imageRef.current);
+		formData.append("upload_preset", "sttruyenxyz");
+		try {
+			const res = await axios.post(
+				"https://api.cloudinary.com/v1_1/sttruyen/image/upload",
+				formData
+			);
+			urlImage = "https:" + res.data.url.split(":")[1];
+		} catch (err) {
+			return;
+		}
+		dispatch(isLoading());
+		try {
+			const data = await axios.post(
+				"/api/marketing/create",
+				{
+					image: urlImage,
+				},
+				{
+					headers: {
+						token: auth.user?.token,
+					},
+				}
+			);
+			toast.success(data?.data?.msg);
+			dispatch(isSuccess());
+		} catch (err) {
+			dispatch(isFailing());
+			return toast.error(err?.response?.data?.msg);
+		}
+	};
+
+	useEffect(() => {
+		if (imageRef.current) {
+			handleCreateMarketingImage();
+		}
+	}, [image]);
+
+	useEffect(() => {}, []);
 	return (
 		<div className="marketing">
 			<div className="marketing_swiper">
@@ -31,19 +82,12 @@ const Marketing = () => {
 						<div className="movie_drop_zone_wrap" {...getRootProps()}>
 							<input {...getInputProps()} />
 							<i className="fa-regular fa-image"></i>
-							<div className="image_create_container">
-								<img src={image} />
+							<div className="image_create_container_img">
+								<img src="" />
 							</div>
 						</div>
 					</div>
 				</div>
-				<MarketingCard />
-				<MarketingCard />
-				<MarketingCard />
-				<MarketingCard />
-				<MarketingCard />
-				<MarketingCard />
-				<MarketingCard />
 				<MarketingCard />
 			</div>
 		</div>
