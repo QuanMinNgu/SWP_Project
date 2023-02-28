214516150e5f8f3b9ec3d3d3ec29d292864751d6
QuangNM <quangnmhe160393@fpt.edu.vn>
2023-02-28 16:23:37 +0700

    updating

--- src/courses/CoursePanel.js
+++ src/courses/CoursePanel.js
@@ -1,41 +1,40 @@
 import React, { useState } from "react";
 import PanelCard from "./PanelCard";
 import "./style.scss";
-const CoursePanel = () => {
-    const [panel, setPanel] = useState(false);
-    const [editPanel, setEditPanel] = useState(false);
-    return (
-        <div className="CoursePanel_wrap">
-            <div
-                onClick={() => {
-                    setPanel(!panel);
-                }}
-                className="CoursePanel_container"
-            >
-                <div className="CoursePanel_head">
-                    <i
-                        style={{
-                            marginRight: "1rem",
-                            fontSize: "1.5rem",
-                            color: "#F3744F",
-                        }}
-                        className={
-                            !panel ? "fa-solid fa-plus" : "fa-solid fa-minus"
-                        }
-                    ></i>
-                    1.Khái niệm kỹ thuật cần biết
-                </div>
-                <div className="CoursePanel_body">3 bài học</div>
-            </div>
-            {panel && (
-                <div>
-                    <PanelCard />
-                    <PanelCard />
-                    <PanelCard />
-                </div>
-            )}
-        </div>
-    );
+const CoursePanel = ({ item, index }) => {
+	const [panel, setPanel] = useState(false);
+	return (
+		<div className="CoursePanel_wrap">
+			<div
+				onClick={() => {
+					setPanel(!panel);
+				}}
+				className="CoursePanel_container"
+			>
+				<div className="CoursePanel_head">
+					<i
+						style={{
+							marginRight: "1rem",
+							fontSize: "1.5rem",
+							color: "#F3744F",
+						}}
+						className={!panel ? "fa-solid fa-plus" : "fa-solid fa-minus"}
+					></i>
+					{index + 1}.{item?.packageTitle}
+				</div>
+				<div className="CoursePanel_body">
+					{item?.numLesson?.length} Lessons
+				</div>
+			</div>
+			{panel && (
+				<div>
+					{item?.numLesson?.map((item, inde) => (
+						<PanelCard index={inde} key={item?.lessonID} item={item} />
+					))}
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default CoursePanel;
