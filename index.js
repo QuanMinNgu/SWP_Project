bdce916d2c8ae8383d0cce90fa8f958d427fc8f5
QuangNM <quangnmhe160393@fpt.edu.vn>
2023-03-16 13:02:28 +0700

    updating

--- index.js
+++ index.js
@@ -6,28 +6,24 @@ const { Server } = require("socket.io");
 app.use(cors());
 const httpServer = http.createServer(app);
 const io = new Server(httpServer, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
+	cors: {
+		origin: "*",
+		methods: ["GET", "POST"],
+	},
 });
-app.get("/", (req, resp) => {
-  resp.send("Ket noi thanh cong");
-});
+
 io.on("connection", (socket) => {
-  console.log(`User: ${socket.id}`);
-  socket.on("disconnect", () => {
-    console.log("User disconnected", socket.id);
-  });
-  socket.on("join_room", (data) => {
-    socket.join(data);
-    console.log(`User : ${socket.id} data : ${data}`);
-  });
-  socket.on("send_mess", (data) => {
-    console.log(data);
-    socket.to(data?.id).emit("recieve", data);
-  });
+	socket.on("disconnect", () => {
+		console.log("User disconnected", socket.id);
+	});
+	socket.on("join_room", (data) => {
+		socket.join(data?.id);
+	});
+	socket.on("send_mess", (data) => {
+		console.log(data);
+		io.to(data?.id).emit("recieve", data);
+	});
 });
 httpServer.listen(7000, () => {
-  console.log("Server in runing");
+	console.log("Server in runing");
 });
